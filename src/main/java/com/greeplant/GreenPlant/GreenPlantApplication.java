b0d4b4b29e26322ee559c683a3285c7f44f75d15
Zhtiking <18352991683@163.com>
2020-07-30 17:23:48 +0800

    项目部署

---
+++ src/main/java/com/greeplant/GreenPlant/GreenPlantApplication.java
@@ -0,0 +1,13 @@
+package com.greeplant.GreenPlant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenPlantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenPlantApplication.class, args);
+	}
+
+}
